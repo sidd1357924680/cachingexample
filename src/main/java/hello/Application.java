914a68558434d3c7a0cb627428c1c24a705589cb
Siddharth <siddharthsriramka@gmail.com>
2022-12-10 16:47:34 +0530

    updated the pom.xml

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import java.io.IOException;
+
 @SpringBootApplication
 @EnableCaching
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         SpringApplication.run(Application.class, args);
+        //openHomePage();
     }
 
+    private static void openHomePage() throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080");
+    }
 }
